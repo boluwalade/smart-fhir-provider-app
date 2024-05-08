06127f2bd441202887ccb27495ca6e3a52200952
Bolu Oluwalade <boluabraham@gmail.com>
2024-05-08 06:46:03 -0400

    :heavy_plus_sign: app component

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import PractitionerLaunch from './lib/components/PractitionerLaunch.svelte';
+
+</script>
+
+<main>
+<PractitionerLaunch> </PractitionerLaunch>
+</main>
+
+<style>
+
+</style>
