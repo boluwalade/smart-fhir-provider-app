966673b695b8cac55b701e232c55d8289dec3e55
Bolu Oluwalade <boluabraham@gmail.com>
2024-05-08 06:42:45 -0400

    :heavy_plus_sign: oauth config

---
+++ src/lib/FhirStores.ts
@@ -0,0 +1,6 @@
+// Constants for OAuth configuration
+export const CLIENT_ID = '9cb055d7-aeda-40b5-8543-b6b2d8102f7d';
+export const REDIRECT_URL = 'http://127.0.0.1:5173';
+export const STATE = '1234567890';
+export const SCOPES = 'openid fhirUser launch user/Patient.read user/Observation.read user/Observation.write'; 
+
